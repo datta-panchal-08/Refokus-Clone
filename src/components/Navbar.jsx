078d7cebed7ef9c1a268aab64915f870025f428c
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-17 20:31:26 +0530

    Navbar component Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='max-w-screen-xl py-6 mx-auto bg-zinc-800'>
+        <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
+        <div className='links'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
