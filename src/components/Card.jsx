00f418af2ade9627cb5546b11312d032a319a344
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-18 22:31:20 +0530

    Created Card Component and Added in Cards Component

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { IoIosArrowRoundForward } from 'react-icons/io'
+const Card = ({width,start,para,hover='none'}) => {
+  return (
+    <div className={` bg-zinc-800 p-5 hover:${hover} rounded-xl ${width} min-h-[30rem] flex flex-col justify-between`}>
+        <div className='w-full'>
+        <div className='w-full flex items-center justify-between'>
+            <h3>one heading</h3>
+            <IoIosArrowRoundForward/>
+            </div>
+            <h1 className='text-3xl font-medium mt-5'>whatever heading</h1>
+
+        </div>       
+        <div className='down w-full'>
+            {
+                start && (
+                    <>
+                    <h1 className='text-6xl font-semibold tracking-tight leading-none ' >start a project</h1>
+                <button className='rounded-full px-5 mt-3 py-2 border-[1px]  border-zinc-50'>Contact Us</button>
+                </>
+                ) 
+            }
+            {para && <p className='text-sm font-medium text-zinc-500 '>Lorem ipsum dolor sit amet consectetur.</p>}
+        </div>
+    </div>
+  )
+}
+
+export default Card
