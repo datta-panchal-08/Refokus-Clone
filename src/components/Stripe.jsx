5d242b0a053467822eeec643f54054c36b2a3dc9
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-18 19:32:17 +0530

    Created Stripe Component

---
+++ src/components/Stripe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Stripe = ({val}) => {
+  return (
+    <div className='w-[16.66%] pr-3 pl-3  py-5 border-t-[1.2px] border-r-[1.2px] border-b-[1.2px] flex justify-between items-center border-zinc-600'>
+        <img  src={val.url} alt="" />
+        <span className='font-semibold'>{val.num}</span>
+    </div>
+  )
+}
+
+export default Stripe
