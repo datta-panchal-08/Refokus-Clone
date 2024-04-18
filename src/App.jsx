380707c81ddc271004298e31b525f1a57b6c7db4
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-18 22:34:21 +0530

    Added Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Navbar from './components/Navbar'
 import Work from './components/Work'
 import Stripes from './components/Stripes'
 import Products from './components/Products'
-
+import Marquees from './components/Marquees'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div className='w-full h-full font-["satoshi"] bg-zinc-900 text-white'>
+    <div className='w-full  font-["satoshi"] bg-zinc-900 text-white'>
       <Navbar/>
       <Work/>
       <Stripes/>
       <Products/>
+      <Marquees/>
+      <Cards/>
+      <Footer/>
     </div>
   )
 }
