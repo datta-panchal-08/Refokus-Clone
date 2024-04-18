03f7be3ac18ec587c82455b226ed7895e7fc54be
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-18 15:18:52 +0530

    setup files for project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white'>
+    <div className='w-full h-screen font-["satoshi"] bg-zinc-900 text-white'>
       <Navbar/>
     </div>
   )
