072f55acb81d67b5c161c07a5e03f4b78b068e66
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-18 22:29:58 +0530

    Created Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full'>
+        <div className='max-w-screen-xl mx-auto py-2 flex gap-7'>
+        {["privacy policy","cookie policy","impressum","terms"].map((item ,index)=> <a className='block cursor-pointer text-zinc-600 mt-2 capitalize'>{item}</a>)}
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
