078c911049e215a5c8cc9d0e97cfe665ada1ca0d
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-19 16:46:49 +0530

    Animation  Completed Marquee Component

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { motion } from "framer-motion"
 
-const Marquee = ({imagesUrls}) => {
+const Marquee = ({ imagesUrls, direction }) => {
   return (
-    <div className='flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden '>
-       {imagesUrls.map(url => <img className='w-[6vw] flex-shrink-0 ' src={url}/>)}
-       {imagesUrls.map(url => <img className='w-[6vw] flex-shrink-0' src={url}/>)}
+    <div className='flex w-full  overflow-hidden '>
+      <motion.div initial={{ x: direction === "left" ? "0" : "-100%" }} animate={{ x: direction === "left" ? "-100%" : "0" }} transition={{ repeat: Infinity, ease: "linear", duration: 10 }} className='flex flex-shrink-0 gap-40 py-10 pr-40'>
+
+        {imagesUrls.map((url, index) => <img key={index} className='w-[6vw] flex-shrink-0 ' src={url} />)}
+
+      </motion.div>
+      <motion.div initial={{ x: direction === "left" ? "0" : "-100%" }} animate={{ x: direction === "left" ? "-100%" : "0" }} transition={{ repeat: Infinity, ease: "linear", duration: 10 }} className='flex flex-shrink-0 gap-40 py-10 pr-40'>
+
+        {imagesUrls.map((url, index) => <img key={index} className='w-[6vw] flex-shrink-0 ' src={url} />)}
+
+      </motion.div>
+
 
     </div>
   )
