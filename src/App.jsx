090779c72e1c3e2bea0f2e9daa549881133a807b
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-19 16:50:20 +0530

    Added Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import Products from './components/Products'
 import Marquees from './components/Marquees'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='w-full  font-["satoshi"] bg-zinc-900 text-white'>
       <Navbar/>
