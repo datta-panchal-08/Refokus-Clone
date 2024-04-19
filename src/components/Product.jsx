0a3f3905473b0f78d4afda5afc77944271f494fd
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-19 16:44:40 +0530

    Animation For  Product Component Completed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,9 @@
 import Button from './Button'
 
-const Product = ({val}) => {
+const Product = ({val,mover,count}) => {
   return (
-    <div className='w-full py-20 text-white'>
-        <div className='max-w-screen-xl flex justify-between items-center mx-auto'>
+    <div className='w-full py-20 h-[23rem] text-white'>
+        <div onMouseEnter={()=>{mover(count)}} className='max-w-screen-xl flex justify-between items-center mx-auto'>
             <h1 className='text-6xl capitalize font-medium'>{val.title}</h1>
             <div className='dets w-1/3 '>
                 <p className='mb-10'>{val.description}</p>
