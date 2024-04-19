256d0e49c6e29c15430cab377e0c6be62933a0f5
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-19 16:47:25 +0530

    Added Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='w-full'>
         <div className='max-w-screen-xl mx-auto py-2 flex gap-7'>
-        {["privacy policy","cookie policy","impressum","terms"].map((item ,index)=> <a className='block cursor-pointer text-zinc-600 mt-2 capitalize'>{item}</a>)}
+        {["privacy policy","cookie policy","impressum","terms"].map((item ,index)=> <a key={index} className='block cursor-pointer text-zinc-600 mt-2 capitalize'>{item}</a>)}
         </div>
 
     </div>
